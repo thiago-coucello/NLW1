437d4edce460fc1ff8e688fcd49631843011ee4b
Thiago Fernandes Coucello da Fonseca <thiago.coucello@gmail.com>
2021-04-04 05:18:57 -0400

    Abstraindo rotas -Points (incompleta) -Itens (todo)

---
+++ server/src/controllers/PointController.ts
@@ -0,0 +1,57 @@
+import knex from "../database/connection"
+import { Request, Response } from "express"
+
+class PointController{
+    public PointController(){}
+
+    async create(request: Request, response: Response){
+        const {
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+            itens
+        } = request.body
+    
+        const trx = await knex.transaction();
+    
+        const insertedIds = await trx("points").insert({
+            image: "sem-imagem",
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf
+        })
+    
+        const point_id = insertedIds[0]
+    
+        const pointItens = itens.map((item_id: number) => {
+            return{
+                item_id,
+                point_id
+            }
+        })
+    
+        await trx("point_itens").insert(pointItens)
+    
+        return response.json({message: "Ponto criado com sucesso", corpo: {
+            image: "sem-imagem",
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+            itens: pointItens
+        }})
+    }
+}
+
+export default PointController
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,69 @@
-import express from 'express';
+import express from "express"
+import knex from "./database/connection"
 
 const routes = express.Router()
 
-routes.get('/', (request, response) =>{
-    return response.json({ message: "Hello World" });
+routes.get("/itens", async (request, response) =>{
+    const itens = await knex("itens").select("*")
+
+    const serializedItens = itens.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}` 
+        }
+    }) 
+
+    return response.json({itens: serializedItens})
 })
 
-export default routes;
+routes.post("/points", async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        itens
+    } = request.body
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx("points").insert({
+        image: "sem-imagem",
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    })
+
+    const point_id = insertedIds[0]
+
+    const pointItens = itens.map((item_id: number) => {
+        return{
+            item_id,
+            point_id
+        }
+    })
+
+    await trx("point_itens").insert(pointItens)
+
+    return response.json({message: "Ponto criado com sucesso", corpo: {
+        image: "sem-imagem",
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        itens: pointItens
+    }})
+})
+
+export default routes
